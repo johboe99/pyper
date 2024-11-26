dfccb444713be114020029a1827306860e1e4f96
Katharina Döhle <kdoehle@gmx.de>
2024-11-26 10:39:11 +0100

    Wcode

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ApplicationRecord
   belongs_to :spot
   belongs_to :user
 
+  has_many :reviews_tags
+
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,4 @@
 class Spot < ApplicationRecord
   belongs_to :category
+  has_many :reviews
 end
