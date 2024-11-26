66cffca886d664b0623845bbeaf176a1c59760fc
Johannes Boehmer <j.boehmer@live.de>
2024-11-26 16:16:02 +0100

    adjusted follower behaviour

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,7 @@ class User < ApplicationRecord
   has_many :spots, through: :reviews
   has_many :followers_as_asker, class_name: "Follower", foreign_key: :asker_id
   has_many :followers_as_receiver, class_name: "Follower", foreign_key: :receiver_id
+  has_many :followings, through: :followers_as_receiver, source: :asker
+  has_many :followers, through: :followers_as_asker, source: :receiver
   has_one_attached :photo
 end
