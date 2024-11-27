dbdfa26f5dae4fcd4894e86dd6bf64cb1a2fcc87
Katharina Döhle <kdoehle@gmx.de>
2024-11-27 17:12:11 +0100

    code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def star_rating(rating, out_of = 5)
+    full_star = '★'
+    empty_star = '☆'
+
+    full_stars = full_star * rating
+    empty_stars = empty_star * (out_of - rating)
+
+    content_tag(:span, full_stars + empty_stars, class: 'star-rating')
+  end
 end
