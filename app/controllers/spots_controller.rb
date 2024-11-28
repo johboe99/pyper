8088dd8c01de900886d2c4694c577d281989e122
Johannes Boehmer <j.boehmer@live.de>
2024-11-28 12:33:02 +0100

    map pin added, only local for now

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -17,7 +17,8 @@ class SpotsController < ApplicationController
         {
           lat: spot.latitude,
           lng: spot.longitude,
-          info_window_html: render_to_string(partial: "info_window", locals: {spot: spot})
+          info_window_html: render_to_string(partial: "info_window", locals: {spot: spot}),
+          marker_html: render_to_string(partial: "marker")
         }
       end
     end
