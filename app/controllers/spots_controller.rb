0bac498e4de86867da1f070e4b1b2cbdbee219ee
Johannes Boehmer <j.boehmer@live.de>
2024-11-29 10:35:02 +0100

    map pins using cloudinary

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -18,7 +18,9 @@ class SpotsController < ApplicationController
           lat: spot.latitude,
           lng: spot.longitude,
           info_window_html: render_to_string(partial: "info_window", locals: {spot: spot}),
-          marker_html: render_to_string(partial: "marker")
+          marker_html: "<div style='background-image: url(#{Cloudinary::Utils.cloudinary_url('pin_pbzvmh.png')});
+                        background-size: contain; background-repeat: no-repeat;
+                        background-position: center; width: 30px; height: 30px;'></div>"
         }
       end
     end
