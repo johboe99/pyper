a07c3ae6e8952976637af3c76e6b7a10b633a945
Johannes Boehmer <j.boehmer@live.de>
2024-11-29 09:14:11 +0100

    added js to toggle the button display for map/list

--- app/javascript/controllers/toggle_map_controller.js
+++ app/javascript/controllers/toggle_map_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-map"
 export default class extends Controller {
-  static targets = ["map", "list"]
+  static targets = ["map", "list", "button"]
 
   connect() {
     console.log("map toggle connected...")
@@ -13,5 +13,10 @@ export default class extends Controller {
     // same for list, needs to be opposite each time, default: map active
     this.mapTarget.classList.toggle("d-none");
     this.listTarget.classList.toggle("d-none");
+    if (this.buttonTarget.innerHTML == '<i class="fa-solid fa-map"></i> Map') {
+      this.buttonTarget.innerHTML = '<i class="fa-solid fa-list"></i> List';
+    } else if (this.buttonTarget.innerHTML == '<i class="fa-solid fa-list"></i> List') {
+      this.buttonTarget.innerHTML = '<i class="fa-solid fa-map"></i> Map';
+    }
   }
 }
