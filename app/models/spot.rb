29ffc3a0a958c01c47a105776398672f9dfbf310
Ajmal Khan <ajmalkhan0413@gmail.com>
2024-12-05 10:34:53 +0100

    name and description updated

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,6 +3,8 @@ class Spot < ApplicationRecord
   has_many :reviews
 
   has_one_attached :photo
+  validates :name, presence: true
+  validates :description, presence: true
   validates :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
